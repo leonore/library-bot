95d0d93cb457d0f4d9935b6fa6c1ac46c557d262
CS01-VM <2247492c@student.gla.ac.uk>
2019-02-13 16:35:59 +0000

    sync across computers

--- script2.sh
+++ script2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 curl https://labstats.campus.gla.ac.uk/api/public/GetPublicApiData/1001 -o /home/ubuntu/leonore/library-bot/new_data.json
-python3 /home/ubuntu/leonore/library-bot/parse.py
-python3 /home/ubuntu/leonore/library-bot/bot.py
+python3 /home/ubuntu/leonore/library-bot/parse2.py
+python3 /home/ubuntu/leonore/library-bot/bot2.py
