027787c55d7115e9ef88290a3b17b0f9da1f5f91
Leonore Papaloizos <2264897V@student.gla.ac.uk>
2020-04-06 12:37:11 +0100

    ignore alternative files

--- script2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-curl https://labstats.campus.gla.ac.uk/api/public/GetPublicApiData/1001 -o /home/ubuntu/leonore/library-bot/new_data.json
-python3 /home/ubuntu/leonore/library-bot/parse2.py
-python3 /home/ubuntu/leonore/library-bot/bot2.py
